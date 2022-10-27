08dafecb567b1df49afdebee347d51e589ec5777
Rémy Dornier <remy.dornier@epfl.ch>
2022-10-27 13:49:03 +0200

    first commit

---
+++ src/main/java/ch/epfl/biop/ArgoLight.java
@@ -0,0 +1,13 @@
+package ch.epfl.biop;
+
+import net.imagej.ImageJ;
+
+public class ArgoLight {
+
+    public static void main(String[] args) {
+        System.out.printf("Test");
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+    }
+}
+
