c9e1554f3d22ff69b811ecdfcf80917f40389a9f
Rémy Dornier <remy.dornier@epfl.ch>
2023-03-10 10:49:29 +0100

    Add SLG482 processing

--- src/main/java/ch/epfl/biop/image/ImageFile.java
+++ src/main/java/ch/epfl/biop/image/ImageFile.java
@@ -14,7 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ImageFile {
-    private final Pattern pattern482 = Pattern.compile("(?<microscope>.*)_o(?<objective>.*)_z(?<zoom>.*)_(?<immersion>.*)_(?<argoslide>.*)_(?<pattern>.*)_d(?<date>[\\d]*)_?(?<series>.*)?\\.(?<extension>.*)");
+    private final Pattern pattern = Pattern.compile("(?<microscope>.*)_o(?<objective>.*)_z(?<zoom>.*)_(?<immersion>.*)_(?<argoslide>.*)_(?<pattern>.*)_d(?<date>[\\d]*)_?(?<series>.*)?\\.(?<extension>.*)");
     private final String imgName;
     private final String imgNameWithoutExtension;
     private String microscope;
@@ -70,7 +70,7 @@ public class ImageFile {
     }
 
     private void parseImageName(String imgName){
-        Matcher matcher = pattern482.matcher(imgName);
+        Matcher matcher = pattern.matcher(imgName);
 
         if(matcher.find()) {
             this.microscope = matcher.group("microscope");
